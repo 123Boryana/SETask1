e0345e82ea2e05a36e826a01c6abf73e0ea1bd91
123Boryana <glavchevaboryana@gmail.com>
2024-09-27 20:12:06 +0300

    Updated first half of the task

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
